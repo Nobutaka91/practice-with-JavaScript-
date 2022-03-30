94476814103691d7c401cb9c292899b508aaa998
otamasan8 <hatarakuotamasan8@outlook.jp>
2022-03-31 00:29:21 +0900

    "属性値の取得"を実装

--- 22_basic_Javascript/main.js
+++ 22_basic_Javascript/main.js
@@ -1,9 +1,15 @@
 'use strict';
 
 let btn = document.getElementById('triggerButton');
-btn.addEventListener('click', function (e){
+btn.addEventListener('click', function (e) {
     // IDによるノードの取得
-    let headerTitle = document.getElementById('headerTitle');
-    // テキストの置換
-    headerTitle.textContent = 'タイトルが変更されました';
+    let inputColorName = document.getElementById('inputColorName');　
+    // 取得したinput要素の入力値を取得
+    let colorNameText = inputColorName.value;
+    // 取得したinput要素のname属性値を取得
+    let inputName = inputColorName.getAttribute('name');
+    // idがshowTextのp要素を取得
+    let showText = document.getElementById('showText');
+    //出力
+    showText.textContent = `name属性: ${inputName}, 入力されたテキスト ${colorNameText}`;
 }, false);
