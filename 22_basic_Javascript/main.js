0924312202fa0dea089cae7c9b51aea255900269
otamasan8 <hatarakuotamasan8@outlook.jp>
2022-03-30 23:27:32 +0900

    テキスト置換を実装した

--- 22_basic_Javascript/main.js
+++ 22_basic_Javascript/main.js
@@ -2,11 +2,9 @@
 
 let btn = document.getElementById('triggerButton');
 btn.addEventListener('click', function (e){
-    // alert('clicked');
     // IDによるノードの取得
     let headerTitle = document.getElementById('headerTitle');
-    // 要素ノードの取得
-    console.log(headerTitle);
-    // 要素ノードのテキストだけを取得
-    console.log(headerTitle.textContent);
+    // テキストの置換
+    headerTitle.textContent = 'タイトルが変更されました';
+
 }, false);
