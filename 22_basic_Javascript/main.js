f673afa3c576aadbc480bce28844b290226e98bd
otamasan8 <hatarakuotamasan8@outlook.jp>
2022-04-01 02:34:16 +0900

    name属性で要素を取得

--- 22_basic_Javascript/main.js
+++ 22_basic_Javascript/main.js
@@ -2,9 +2,12 @@
 
 let btn = document.getElementById('triggerButton');
 btn.addEventListener('click', function (e) {
-    let elements = document.getElementsByTagName('p');
+    let elements = document.getElementsByName('inputText');
     console.log(elements);
-    for (let i = 0; i < elements.length; i++){
-        console.log(elements[i].textContent);
-    }
+    console.log(elements[0].value);
 }, false);
+ 
+
+// name属性で要素を取得するにはgetElementsByNameメソッドを使用する
+// Elementsが複数形であることに注意
+// 戻り値は配列形式のような形で返るのでinput要素に入力された値を取り出すためにはelementsに[0]をつけ、valueプロパティを使用する。
