5ae7fecf8f954936edc267c80b2a4e92a6d77264
otamasan8 <hatarakuotamasan8@outlook.jp>
2022-04-01 01:57:00 +0900

    タグ名で要素を取得

--- 22_basic_Javascript/main.js
+++ 22_basic_Javascript/main.js
@@ -1,32 +1,10 @@
 'use strict';
 
-// class属性を追加・更新 (赤)
-let setRedClassButton = document.getElementById('setRedClassButton');
-setRedClassButton.addEventListener('click', function (e) {
-    let showText = document.getElementById('showText');
-    showText.setAttribute('class', 'red');
+let btn = document.getElementById('triggerButton');
+btn.addEventListener('click', function (e) {
+    let elements = document.getElementsByTagName('p');
+    console.log(elements);
+    for (let i = 0; i < elements.length; i++){
+        console.log(elements[i].textContent);
+    }
 }, false);
- 
-// class属性を追加・更新 (青)
-let setBlueClassButton = document.getElementById('setBlueClassButton');
-setBlueClassButton.addEventListener('click', function (e) {
-    let showText = document.getElementById('showText');
-    showText.setAttribute('class', 'blue');
-}, false);
- 
-// 属性の削除
-let removeClassButton = document.getElementById('removeClassButton');
-removeClassButton.addEventListener('click', function (e) {
-    let showText = document.getElementById('showText');
-    showText.removeAttribute('class');
-}, false);
-
-
-
-
-// 属性を書き換えたい場合は
-// setAttributeメソッドを使用する
-
-
-// 属性を削除したい場合は
-// removeAttributeメソッドを使用する
