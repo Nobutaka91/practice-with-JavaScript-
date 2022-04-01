360e683537552ed8bf5eb662853eb8060801fb4b
otamasan8 <hatarakuotamasan8@outlook.jp>
2022-04-02 01:40:02 +0900

    クラス名で要素を取得

--- 22_basic_Javascript/main.js
+++ 22_basic_Javascript/main.js
@@ -2,12 +2,12 @@
 
 let btn = document.getElementById('triggerButton');
 btn.addEventListener('click', function (e) {
-    let elements = document.getElementsByName('inputText');
+    let elements = document.getElementsByClassName('foo');
     console.log(elements);
-    console.log(elements[0].value);
+    for (let i = 0; i < elements.length; i++) { 
+        console.log(elements[i].textContent);
+    }
 }, false);
- 
-
-// name属性で要素を取得するにはgetElementsByNameメソッドを使用する
+// クラス名で要素を取得するにはgetElementsByClassNameメソッドを使用する
 // Elementsが複数形であることに注意
-// 戻り値は配列形式のような形で返るのでinput要素に入力された値を取り出すためにはelementsに[0]をつけ、valueプロパティを使用する。
+// 戻り値は配列形式のような形で返るので,要素を取得するためにはelementsに[i]をつけ、textContentプロパティを使用する。
