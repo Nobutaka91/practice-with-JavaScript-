27ffc52a4622d941fe5982a1210793f63f0c2d89
otamasan8 <hatarakuotamasan8@outlook.jp>
2022-04-02 02:34:55 +0900

    セレクタで要素を取得

--- 22_basic_Javascript/main.js
+++ 22_basic_Javascript/main.js
@@ -2,12 +2,16 @@
 
 let btn = document.getElementById('triggerButton');
 btn.addEventListener('click', function (e) {
-    let elements = document.getElementsByClassName('foo');
+    // セレクターを使ってあるクラスに一致する最初の要素を取得するにはquerySelectorメソッドを使う。
+    let element = document.querySelector('.foo');
+    console.log(element);
+    console.log(element.textContent);
+
+    // 複数取得するにはquerySelectorAllメソッドを使用する
+    let elements = document.querySelectorAll('.foo');
     console.log(elements);
-    for (let i = 0; i < elements.length; i++) { 
+    for (let i = 0; i < elements.length; i++) {
         console.log(elements[i].textContent);
-    }
-}, false);
-// クラス名で要素を取得するにはgetElementsByClassNameメソッドを使用する
-// Elementsが複数形であることに注意
-// 戻り値は配列形式のような形で返るので,要素を取得するためにはelementsに[i]をつけ、textContentプロパティを使用する。
+     }
+ }, false);
+
