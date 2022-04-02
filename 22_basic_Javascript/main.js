d0ae2f939f6d622961a6191e5cf66e9d73184030
otamasan8 <hatarakuotamasan8@outlook.jp>
2022-04-03 03:24:12 +0900

    changeイベントを利用してイベントハンドラを実行

--- 22_basic_Javascript/main.js
+++ 22_basic_Javascript/main.js
@@ -1,20 +1,22 @@
 'use strict';
 
-const sayHelloButton = document.getElementById('sayHelloButton');
-function sayHelloEventHandler(e) {
-    console.log('Hello');
+// イベントとイベントハンドラ
+function handleChange() {
+    // 「すべてチェック」の要素を取得
+    const checkAll = document.getElementById('checkAll');
+    // name属性がlanguageのチェックボックス要素を取得
+    const checkBoxes = document.getElementsByName('language');
+    // name属性がlanguageのチェックボックス要素をループ処理
+    for (let i = 0; i < checkBoxes.length; i++) {
+
+        // 各チェックボックスのチェック状態を、
+        // 「すべてにチェックボックス」の状態に合わせる
+        checkBoxes[i].checked = checkAll.checked;
+    }
 }
-sayHelloButton.addEventListener('click', sayHelloEventHandler, false);
 
-
-const removeEventHandlerButton = document.getElementById('removeEventHandlerButton');
-function removeEventHandler(e) { 
-    console.log('remove');
-    sayHelloButton.removeEventListener('click', sayHelloEventHandler, false);
-}
-removeEventHandlerButton.addEventListener('click', removeEventHandler, false);
-
-
+// checkAllボタンのchangeイベントのイベントハンドラを登録
+checkAll.addEventListener('change', handleChange, false);
 
 
 
