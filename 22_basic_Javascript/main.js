5ec7108a02b479458f6ea59eb09d7a588a8acfae
otamasan8 <hatarakuotamasan8@outlook.jp>
2022-04-02 22:43:05 +0900

    イベントハンドラの削除

--- 22_basic_Javascript/main.js
+++ 22_basic_Javascript/main.js
@@ -1,9 +1,23 @@
 'use strict';
 
-// const sayHelloButton = document.getElementById('sayHelloButton');
-// sayHelloButton.addEventListener('click',  (e) =>{
-//     console.log('Hello!');
-// }, false);
+const sayHelloButton = document.getElementById('sayHelloButton');
+function sayHelloEventHandler(e) {
+    console.log('Hello');
+}
+sayHelloButton.addEventListener('click', sayHelloEventHandler, false);
+
+
+const removeEventHandlerButton = document.getElementById('removeEventHandlerButton');
+function removeEventHandler(e) { 
+    console.log('remove');
+    sayHelloButton.removeEventListener('click', sayHelloEventHandler, false);
+}
+removeEventHandlerButton.addEventListener('click', removeEventHandler, false);
+
+
+
+
+
 
 
 
