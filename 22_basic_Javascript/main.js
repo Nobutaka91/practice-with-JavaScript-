d069e2e4c405e2409ad400bd2950d29e22527215
otamasan8 <hatarakuotamasan8@outlook.jp>
2022-04-02 21:16:28 +0900

    windowsオブジェクトを実装

--- 22_basic_Javascript/main.js
+++ 22_basic_Javascript/main.js
@@ -1,17 +1,27 @@
 'use strict';
 
+// Windowオブジェクトとは
+// ブラウザの画面に関係するプロパティやメソッドを束ねておく入れ物のようなもの。
+// グローバルオブジェクトのため、どこからでも呼び出せてすぐに使える。
+
+
+// コンソール出力
+// window.console.log('Hello');
+
+// 警告表示
+// window.alert('Are you OK?');
+
+// 確認ダイアログ
+// window.confirm('Are you OK?');
+
+// 別のWindowを開く
+// window.open('https://www.google.co.jp');
+
+// Windowを閉じる
+// window.close();
+
+// スクロール
 let btn = document.getElementById('triggerButton');
 btn.addEventListener('click', function (e) {
-    // セレクターを使ってあるクラスに一致する最初の要素を取得するにはquerySelectorメソッドを使う。
-    let element = document.querySelector('.foo');
-    console.log(element);
-    console.log(element.textContent);
-
-    // 複数取得するにはquerySelectorAllメソッドを使用する
-    let elements = document.querySelectorAll('.foo');
-    console.log(elements);
-    for (let i = 0; i < elements.length; i++) {
-        console.log(elements[i].textContent);
-     }
+    window.scroll(0, 300);
  }, false);
-
