599b12c9edaf458df07a5142d0b28d8af40e1422
otamasan8 <hatarakuotamasan8@outlook.jp>
2022-04-03 18:09:53 +0900

    カウンターを実装

--- 22_basic_Javascript/main.js
+++ 22_basic_Javascript/main.js
@@ -1,37 +1,24 @@
 'use strict';
 
-// イベントとイベントハンドラ
-function handleChange() {
-    // 「すべてチェック」の要素を取得
-    const checkAll = document.getElementById('checkAll');
-    // name属性がlanguageのチェックボックス要素を取得
-    const checkBoxes = document.getElementsByName('language');
-    // name属性がlanguageのチェックボックス要素をループ処理
-    for (let i = 0; i < checkBoxes.length; i++) {
-
-        // 各チェックボックスのチェック状態を、
-        // 「すべてにチェックボックス」の状態に合わせる
-        checkBoxes[i].checked = checkAll.checked;
-    }
+let num = 0;
+function updateCounter(n) {
+    const counter = document.getElementById('counter');
+    counter.textContent = n;
 }
 
-// checkAllボタンのchangeイベントのイベントハンドラを登録
-checkAll.addEventListener('change', handleChange, false);
+
+function countUp() {
+    num++;
+    updateCounter(num);
+}
+
+function reset() {
+    num = 0;
+    updateCounter(num);
+ }
+
+countUpButton.addEventListener('click', countUp, false);
+resetButton.addEventListener('click', reset, false);
 
 
-
-
-
-
-// 「イベントハンドラ」
-// イベントに対して行う処理のこと
-// 例) コンソールにHelloと出力する処理
-
-
-// 「イベントリスナ」
-// イベントとイベントハンドラを紐づけているもの
-
-
-// 「イベント駆動型プログラミング」(イベントドリブン)
-// 何らかの、イベントが発生したら、何らかのプログラミングが動作するようプログラミングを書く手法のことを指す
 
